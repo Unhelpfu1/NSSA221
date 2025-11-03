cdd3cef2f3ce43d681d1f56596cc75d87c1813d4
Unhelpfu1 <krogstad.josh@gmail.com>
2025-11-03 15:20:47 -0500

    added search for ips

--- attacker_report.py
+++ attacker_report.py
@@ -8,5 +8,22 @@ import os
 # Clear terminal
 os.system("clear")
 
+# Keep track of all ips that fail a login
 ip_appearances = dict()
-for entry in 
+
+# Check every log entry
+for entry in open("/home/student/Downloads/syslog.log"):
+  # Every failed login will say "Failed password"
+  if "Failed password" in entry:
+    # Regex for ip address
+    match = re.search(r"\d{1,3}\.\d+\.\d+\.\d{1,3}")
+    ip_address = match.group(0)
+    # If ip already exists in dict
+    if ip_appearances[ip_address] != None:
+      # Increment count by 1
+      ip_appearances[ip_address]++
+    else:
+      # Add ip to list at 1 appearance
+      ip_appearances[ip_address] = 1
+
+print(ip_appearances) # DEBUG
