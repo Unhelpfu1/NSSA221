f74e7a78430984621b9019319d362b9ddf1c29e3
Unhelpfu1 <krogstad.josh@gmail.com>
2025-11-06 01:08:34 -0500

    updated sort method

--- attacker_report.py
+++ attacker_report.py
@@ -27,19 +27,19 @@ for entry in open("/home/student/Downloads/syslog.log"):
       # Add ip to list at 1 appearance
       ip_appearances[ip_address] = 1
 
-print(ip_appearances) # DEBUG
+#print(ip_appearances) # DEBUG
 
-test_country = geolite2.lookup('8.8.8.8') # DEBUG
-print(test_country.country) # DEBUG
+#test_country = geolite2.lookup('8.8.8.8') # DEBUG
+#print(test_country.country) # DEBUG
 
 # Set up headers of list to be printed
 to_print = "COUNT\tIP ADDRESS\tCOUNTRY\n"
 # Sort by number of appearances
-for key in sorted(ip_appearances):
+for ip_address in sorted(ip_appearances.items(), key=lambda kv: (kv[1], kv[0])):
   # If failed logins >= 10
-  if (ip_appearances[key] >= 10):
+  if (ip_address[1] >= 10):
     # Add num failed logins, source ip, and ip country to print
-    to_print += ip_appearances[key] + "\t" + key + "\t" + geolite2.lookup(key).country + "\n"
+    to_print += str(ip_address[1]) + "\t" + str(ip_address[0]) + "\t" + geolite2.lookup(ip_address[0]).country + "\n"
 
 # Print the chart made above
 print(to_print)
